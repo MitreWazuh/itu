b5bc17a668f52082d5a77fd53f6e7dcf111f6643
stoksoz1 <48630654+stoksoz1@users.noreply.github.com>
2024-04-27 16:28:10 +0300

    Update t-pot.sh

--- t-pot/t-pot.sh
+++ t-pot/t-pot.sh
@@ -1,8 +1,11 @@
 uname -a
-Linux tpot2 5.10.0-28-cloud-amd64 #1 SMP Debian 5.10.209-2 (2024-01-31) x86_64 GNU/Linux
 
+sudo apt update -y && sudo apt upgrade -y
 
-sudo apt-get install git -y
-git clone https://github.com/dtag-dev-sec/tpotce
-cd tpotce/iso/installer/
-./install.sh --type=user
+env bash -c "$(curl -sL https://github.com/telekom-security/tpotce/raw/master/install.sh)"
+sudo reboot
+
+firewall setup
+64295  - myIP  -  Allow
+64297  - myIP  - Allow
+1-64200 - anyIP - Allow
