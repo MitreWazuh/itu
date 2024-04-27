1eb0a5a6cf8066143aacf7ae59d95fd831e6c6a7
stoksoz1 <48630654+stoksoz1@users.noreply.github.com>
2024-04-27 22:59:53 +0300

    Create setup.sh

---
+++ invoke-atomicredteam/setup.sh
@@ -0,0 +1,8 @@
+//https://github.com/redcanaryco/invoke-atomicredteam/wiki/Docker-Containers
+
+
+git clone https://github.com/redcanaryco/invoke-atomicredteam.git
+cd invoke-atomicredteam/docker
+# edit Dockerfile
+docker build -t invoke-atomicredteam:latest .
+docker run -it invoke-atomicredteam:latest
