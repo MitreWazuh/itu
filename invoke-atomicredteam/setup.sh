ea3253667c16bfeb3368e92684d592e3d948796b
stoksoz1 <48630654+stoksoz1@users.noreply.github.com>
2024-04-27 23:55:05 +0300

    Update setup.sh

--- invoke-atomicredteam/setup.sh
+++ invoke-atomicredteam/setup.sh
@@ -1,6 +1,7 @@
 https://github.com/redcanaryco/invoke-atomicredteam/wiki/Docker-Containers
 
-docker run -it redcanary/invoke-atomicredteam:latest
+docker run -it -v /home/stoksoz/1.csv:/1.csv redcanary/invoke-atomicredteam:latest
+
 
 Test
 Invoke-AtomicTest T1003.008
