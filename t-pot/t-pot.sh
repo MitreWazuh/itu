2ff822878c7e2e3ae8985b6aba0f97e0cb6a8c6a
stoksoz1 <48630654+stoksoz1@users.noreply.github.com>
2024-04-27 17:18:48 +0300

    Update t-pot.sh

--- t-pot/t-pot.sh
+++ t-pot/t-pot.sh
@@ -8,6 +8,13 @@ env bash -c "$(curl -sL https://github.com/telekom-security/tpotce/raw/master/in
 sudo reboot
 
 firewall setup
-64295  - myIP  -  Allow
-64297  - myIP  - Allow
-1-64200 - anyIP - Allow
+64295  myIP  Allow
+64297  myIP  Allow
+1-64200  anyIP  Allow
+
+wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.7.3-1_amd64.deb && sudo WAZUH_MANAGER='20.124.88.249' WAZUH_AGENT_GROUP='default' WAZUH_AGENT_NAME='tpot' dpkg -i ./wazuh-agent_4.7.3-1_amd64.deb
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable wazuh-agent
+sudo systemctl start wazuh-agent
