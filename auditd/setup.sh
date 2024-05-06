4633d11febaa14bd5a279827dedcd093de205b79
stoksoz1 <48630654+stoksoz1@users.noreply.github.com>
2024-05-06 09:22:57 +0300

    Update setup.sh

--- auditd/setup.sh
+++ auditd/setup.sh
@@ -3,3 +3,16 @@
 
 sudo apt-get install auditd audispd-plugins
 
+
+
+
+// on wazuh agent ossec.conf add this
+// /var/ossec/etc/ossec.conf
+  <localfile>
+    <log_format>syslog</log_format>
+    <location>/var/log/audit/audit.log</location>
+  </localfile>
+
+
+// add audit rules on agent
+// /etc/audit/rules.d/
