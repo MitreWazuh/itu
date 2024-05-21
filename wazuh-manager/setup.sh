2a86f56e133412b77b815f2f42fc73479149a63e
mitre_wazuh <48630654+MitreWazuh@users.noreply.github.com>
2024-05-21 17:17:22 +0300

    Update setup.sh

--- wazuh-manager/setup.sh
+++ wazuh-manager/setup.sh
@@ -3,6 +3,9 @@ uname -a
 
 curl -sO https://packages.wazuh.com/4.7/wazuh-install.sh && sudo bash ./wazuh-install.sh -a
 
+
+FW setup
+/////////////////////////////
 443  anyIp  Allow
 1514  anyIp  Allow  
 1515  anyIp  Allow
