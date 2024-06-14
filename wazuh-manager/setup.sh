d092119dd68b9b6306cd47913e12c1b74d65146b
MitreWazuh <48630654+MitreWazuh@users.noreply.github.com>
2024-06-14 10:10:23 +0300

    Update setup.sh

--- wazuh-manager/setup.sh
+++ wazuh-manager/setup.sh
@@ -1,5 +1,10 @@
 uname -a
 
+Standard D2s v3 (2 vcpus, 8 GiB memory)
+Source image publisher :canonical
+Source image offer : 0001-com-ubuntu-server-jammy
+Source image plan: 22_04-lts-gen2
+
 
 curl -sO https://packages.wazuh.com/4.7/wazuh-install.sh && sudo bash ./wazuh-install.sh -a
 
